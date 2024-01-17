06f1be00579ad4ecec71f2ebdbcf37bed26a5f9a
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-01-17 23:01:43 +0000

    Solution 6: Remove `rm(list = ls())`

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,17 +1,12 @@
 # Load packages
 library(readr)
-
-# Delete everything in current global environment
-rm(list = ls())
-
-# Set working directory
-setwd("~/Code/milanwiedemann/phc-coding-club/")
+library(here)
 
 # Load data
-patients <- read_csv("dummy_data/patients.csv",
+patients <- read_csv(here("dummy_data", "patients.csv"),
   col_types = list(patient_id = "c")
 )
 
-clinical_events <- read_csv("dummy_data/clinical_events.csv",
+clinical_events <- read_csv(here("dummy_data", "clinical_events.csv"),
   col_types = list(patient_id = "c", snomedct_code = "c", ctv3_code = "c")
 )
