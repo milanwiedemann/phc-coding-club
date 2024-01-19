b3de6fad3439b69d81addfc06254bc400245f1ee
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-01-19 15:33:38 +0000

    Solution 3: Run `styler::style_dir(".")`

--- scripts/summarise_patients.R
+++ scripts/summarise_patients.R
@@ -1,20 +1,20 @@
 # Load packages
 library(dplyr)
-                        library(readr)
-     library(lubridate)
-   library(here)
-                                          library(fs)
+library(readr)
+library(lubridate)
+library(here)
+library(fs)
 
 # First run the script "scripts/load_data.R"
 
 # Summarise patients data
 patients_summary <- patients |>
-                      mutate(age= year(as.period(                                       interval(date_of_birth,            today())))) |>
+  mutate(age = year(as.period(interval(date_of_birth, today())))) |>
   summarise(
-                    age_min = min(age,na.rm =TRUE),
-    age_max = max(age, na.rm =    TRUE)            ,
-           n_sex_female                = sum(sex == "female", na.rm = TRUE),
-    n_sex_male = sum(sex == "male",            na.rm               = TRUE),
+    age_min = min(age, na.rm = TRUE),
+    age_max = max(age, na.rm = TRUE),
+    n_sex_female = sum(sex == "female", na.rm = TRUE),
+    n_sex_male = sum(sex == "male", na.rm = TRUE),
     n_sex_na = sum(is.na(sex))
   )
 
@@ -22,15 +22,10 @@ patients_summary <- patients |>
 dir_create(here("results"))
 
 # Write results
-write_csv(patients_summary, 
-  here(              "results",
-                          
-                     
-                     
-                     
-                     
-                     
-                         "patients_summary.csv"
-                     )
-                                        )
-
+write_csv(
+  patients_summary,
+  here(
+    "results",
+    "patients_summary.csv"
+  )
+)
