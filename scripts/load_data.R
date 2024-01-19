ec9b02e35b5fe3eb770e083dbd8b77a4a5c4cea0
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-01-19 15:32:12 +0000

    Solution 1: Use `here` package instead of `setwd()`

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,17 +1,15 @@
 # Load packages
 library(readr)
+library(here)
 
 # Delete everything in current global environment
 rm(list = ls())
 
-# Set working directory
-setwd("~/Code/milanwiedemann/phc-coding-club/")
-
 # Load data
-patients <- read_csv("dummy_data/patients.csv",
+patients <- read_csv(here("dummy_data", "patients.csv"),
   col_types = list(patient_id = "c")
 )
 
-clinical_events <- read_csv("dummy_data/clinical_events.csv",
+clinical_events <- read_csv(here("dummy_data", "clinical_events.csv"),
   col_types = list(patient_id = "c", snomedct_code = "c", ctv3_code = "c")
 )
